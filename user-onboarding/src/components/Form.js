5aec15fcccd367d883b82c166aa3a37a5a2f8354
Desiree Morris <desiquinncompany@gmail.com>
2019-08-07 18:59:41 -0400

    step 2 compelete, implemented form validation and error messaging

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
 
-const UserForm = () => {
+const UserForm = ({errors, touched}) => {
 
     // Name
     // Email
@@ -13,31 +14,21 @@ const UserForm = () => {
         <div>
             <h1> OnBoarding Form </h1>
             <Form>
-                <Field
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                />
+                <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (<p className="error">{errors.name}</p>)}
 
-                <Field
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                />
+                <Field type="email" name="email" placeholder="Email" />
+                {touched.email && errors.email && (<p className="error">{errors.email}</p>)}
 
-                <Field
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                />
+                <Field type="password" name="password" placeholder="Password" />
+                {touched.password && errors.password && (<p className="error">{errors.password}</p>)}
+                
                 <label>
                     Terms of Service
-                    <Field
-                        type="checkbox"
-                        name="terms"
-                        placeholder="Terms of Service"
-                    />
+                    <Field type="checkbox" name="terms" placeholder="Terms of Service" />
+                    {touched.terms && errors.terms && (<p className="error">{errors.terms}</p>)}
                 </label>
+
                 <button type="submit">Submit</button>
             </Form>
         </div>
@@ -54,10 +45,17 @@ const FormikUserForm = withFormik({
         };
     },
 
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('name is required'),
+        email: Yup.string().email().required('email is required'),
+        password: Yup.string().required('password is required'),
+        terms: Yup.boolean()
+    }),
+
     handleSubmit(values){
         console.log(values)
     }
-    
+
 })(UserForm);
 
 export default FormikUserForm;
